b4f0ddf5dca52e166af4884489c15adab44521ce
Ziwa Mukungu <ziwabigboy@gmail.com>
2017-12-15 16:26:42 -0500

    gave enemy movement

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = 0;
+    this.speed = 100;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
