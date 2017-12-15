7f92fb0a13aeedcac21538ed6edb449640dffc0d
Ziwa Mukungu <ziwabigboy@gmail.com>
2017-12-15 16:09:09 -0500

    created Player class

--- js/app.js
+++ js/app.js
@@ -24,12 +24,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+}
 
+Player.prototype.update = function(dt) {
+    
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+    switch(key){
+        case 'left':
+            break;
+        case 'right':
+            break;
+        case 'down':
+            break;
+        case 'up':
+            break;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 
 
 // This listens for key presses and sends the keys to your
